6a8f622a77ecc226dc3d6462a5f7d1a8b6a9f33c
gnats26 <gnats26@yahoo.com>
2017-01-31 02:47:21 +0530

    "set bounds for fireball"

--- routes/fireball.js
+++ routes/fireball.js
@@ -2,25 +2,56 @@
 var display = require('./display');
 var self = module.exports = {
 
+
+
     createfireball: function () {
         var fireball = {};
         //fireball.x = Math.floor(Math.random() * 100 + 100);
-
-        fireball.x=1;
-        fireball.y = 1;
+        fireball.x=Math.floor(Math.random()*(1491)) + 500;
+        // fireball.x=750;
+        // fireball.y =Math.floor(Math.random()*790)+20;
+        fireball.y=800;
         return fireball;
     },
 
-    move_fireball: function (Game) {
-         Game.fireball.x++;
-         for(var i = 1; i<22; i++)
-         {
-        Game.fireball.y= Math.pow(i,2);
-        //move_fireball(Game);
-         }
+    allow_boundary_for_fireball: function(Game)
+    {
+        var a = Game.fireball.x;
+        var b = Game.fireball.y;
+        var c = Math.hypot(1500,800);
+        console.log("A:"+ a + "B: " + b);
+        if(((Math.hypot(a,b))<c)&& a>0 && b>0)
+        {
+            self.move_fireball(Game);
+            
+        }
+    },
 
-
-
+    move_fireball: function (Game)
+    {
+         
+         
+             if((Game.fireball.x>750)&&(Game.fireball.y<400))
+            {
+             Game.fireball.x++;
+             Game.fireball.y--;
+            }
+            else if((Game.fireball.x>750)&&(Game.fireball.y>400))
+            {
+             Game.fireball.x++;
+            Game.fireball.y++;
+            }
+            else if((Game.fireball.x<750)&&(Game.fireball.y>400))
+            {
+            Game.fireball.x--;
+            Game.fireball.y++;
+            }
+            else if ((Game.fireball.x<750)&&(Game.fireball.y<400))
+            {
+            Game.fireball.x--;
+            Game.fireball.y--;
+            }
+         
      },
 
     assignfireballposition: function (Game) {
@@ -29,9 +60,11 @@ var self = module.exports = {
         if (Game.generate_fireball) {
             var fireball_pack = [];
             if (Game.fireball.y > 0 ) {
-                //self.move_fireball(Game)
-                Game.fireball.x++;
-                Game.fireball.y=2*(Game.fireball.x);
+            //    Game.fireball.x;
+            // Game.fireball.y;
+                self.allow_boundary_for_fireball(Game);
+                console.log("X coord: " + Game.fireball.x + "** Y coord: " + Game.fireball.y);
+                
                 fireball_pack.push({
                     x:Game.fireball.x,
                     y:Game.fireball.y
